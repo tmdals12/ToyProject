e0e233835d0f2b1cd19af8c3846bfc5deef46eef
tmdals12 <hahasm3136@gmail.com>
2024-09-21 17:36:38 +0900

    Unity Scirpt

---
+++ ToyProject/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityLearn
+{
+
+    public class Player : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Start함수 있습니다");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("Update함수 있습니다");
+        }
+    }
+
+
+}
+
